0c8c308e76eb695d5c2108ea1c3a8c61f00cd643
Mukles <Mukleshossen1763@gmail.com>
2022-08-12 15:43:07 +0600

    done

--- src/Components/Helper/socialButton.js
+++ src/Components/Helper/socialButton.js
@@ -2,8 +2,8 @@ import Buton from "./button";
 
 const SocialButton = () => {
   return (
-    <div className="socail__buttons hidden xl:flex flex-col absolute right-9 top-1/2">
-      <Buton text={"FB"} />
+    <div className="socail__buttons hidden xl:flex flex-col absolute right-9 bottom-28 space-y-4">
+      <Buton text={"FB."} />
       <Buton text={"DR."} />
       <Buton text={"BE."} />
       <Buton text={"TW."} />
