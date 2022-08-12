d0658a5ab8df5ffdbbdb23c8db26b035e4297f09
Mukles <Mukleshossen1763@gmail.com>
2022-08-12 15:27:54 +0600

    done

--- src/Components/Helper/service/card.js
+++ src/Components/Helper/service/card.js
@@ -7,7 +7,7 @@ const Card = ({ svg, title, link }) => {
       whileInView={{ opacity: 1, x: 0 }}
       viewport={{ once: false, amount: 0.6 }}
       transition={{ duration: 0.8 }}
-      className="bg-grayWhite rounded md:shadow-sm hover:shadw-[0 0 10px rgb(0 0 0 / 10%)] flex flex-col justify-start space-y-4 px-8 py-12"
+      className="services-card bg-grayWhite rounded md:shadow-sm hover:shadw-[0 0 10px rgb(0 0 0 / 10%)] flex flex-col justify-start space-y-4 px-8 py-12"
     >
       <div className="card-img">
         <img className="h-12" src={svg} alt="" />
@@ -17,9 +17,9 @@ const Card = ({ svg, title, link }) => {
       </h3>
       <button className="flex space-x-5 text-left text-lg leading-[30px] font-normal hover:text-ornage text-semiBlack">
         <span>{link}</span>
-        <span className="learn-more">
-          <i className="fa-solid fa-arrow-right-long hover:text-3xl"></i>
-        </span>
+        <p className="learn-more">
+          <span></span>
+        </p>
       </button>
     </motion.div>
   );
